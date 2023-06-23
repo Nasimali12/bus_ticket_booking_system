fa322351e070c65de71bc1a61364dd92d173c330
MD NASIM ALI LASKAR <nasimali@tuespotsolutions.com>
2023-06-23 15:17:05 +0530

    ticketFare DAO COMPLETE

--- src/main/java/busTicketBookingSystem/dao/TicketFareDAO.java
+++ src/main/java/busTicketBookingSystem/dao/TicketFareDAO.java
@@ -9,28 +9,77 @@ import busTicketBookingSystem.dto.TicketFareDTO;
 import busTicketBookingSystem.exception.TicketBookingException;
 
 public class TicketFareDAO {
-	
-	 private EntityManagerFactory entityManagerFactory;
+    private EntityManagerFactory entityManagerFactory;
 
-	public TicketFareDAO() {
-		entityManagerFactory=Persistence.createEntityManagerFactory("dev");
-	}
-	
-	public void addTicketFare(TicketFareDTO ticketFareDTO) throws TicketBookingException {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		EntityTransaction entityTransaction = entityManager.getTransaction();
-		try {
-			entityTransaction.begin();
-			entityManager.persist(ticketFareDTO);
-			entityTransaction.commit();
-		} catch (Exception e) {
-			if(entityTransaction !=null) {
-				entityTransaction.rollback();
-			}
-			e.printStackTrace();
-			throw new TicketBookingException("Failed to add ticket fare: " + e.getMessage());
-		}finally {
-			entityManager.close();
-		}
-	} 
+    public TicketFareDAO() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("your-persistence-unit-name");
+    }
+
+    public void addTicketFare(TicketFareDTO ticketFare) throws TicketBookingException {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.persist(ticketFare);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            throw new TicketBookingException("Failed to add ticket fare: " + e.getMessage());
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public void updateTicketFare(TicketFareDTO ticketFare) throws TicketBookingException {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.merge(ticketFare);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            throw new TicketBookingException("Failed to update ticket fare: " + e.getMessage());
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public void deleteTicketFare(int ticketFareId) throws TicketBookingException {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            TicketFareDTO ticketFare = entityManager.find(TicketFareDTO.class, ticketFareId);
+            entityManager.remove(ticketFare);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            throw new TicketBookingException("Failed to delete ticket fare: " + e.getMessage());
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public TicketFareDTO getTicketFareById(int ticketFareId) throws TicketBookingException {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            return entityManager.find(TicketFareDTO.class, ticketFareId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new TicketBookingException("Failed to get ticket fare by ID: " + e.getMessage());
+        } finally {
+            entityManager.close();
+        }
+    }
 }
+
